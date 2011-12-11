c80ca0e1f38eeeae73fe3dc50025b5735d2f6681
Fabio Trentini <trentas@gmail.com>
2011-12-11 03:46:04 -0200

    now supports a configuration file

--- contact-form.py
+++ contact-form.py
@@ -1,46 +1,48 @@
-from bottle import get, post, request, run, static_file, redirect
+from bottle import get, post, request, run, static_file, redirect, abort
+import ConfigParser
 import smtplib
 from email.MIMEText import MIMEText
 
-STATIC_ROOT_PATH = "static/"
-BIND_ADDRESS = "localhost"
-BIND_PORT = "8080"
+CONFIG_FILE = 'contact-form.conf'
 
-MAIL_RECIPIENT = 'my_dest_email@gmail.com'
-REDIRECT_AFTER_SUCESS = 'http://google.com/'
-
-GMAIL_LOGIN = 'my_gmail_login@gmail.com'
-GMAIL_PASSWORD = 'my_gmail_password'
+config = ConfigParser.ConfigParser()
+config.read(CONFIG_FILE)
+bind_address = config.get('main', 'bind_address')
+bind_port = config.get('main', 'bind_port')
+static_document_root = config.get('main', 'static_document_root')
 
 @get('/')
 @get('/contact/:destination')
 def server_file(destination='dummy'):
-    return static_file(filename="index.html", root=STATIC_ROOT_PATH)
+    return static_file(filename="index.html", root=static_document_root)
 
 @post('/contact/:destination')
 def send_form(destination):
+	gmail_login = config.get(destination, 'gmail_login')
+	gmail_password = config.get(destination, 'gmail_password')
+	gmail_rcpt_to = config.get(destination, 'gmail_rcpt_to')
+	redirect_after_sucess = config.get(destination, 'redirect_after_sucess')
 	form_name = request.forms.get('name')
 	form_email = request.forms.get('email')
 	form_subject = request.forms.get('subject')
 	form_body = request.forms.get('comments')
 	mail_from = '%s <%s>' % (form_name, form_email)
-	rcpt_to = MAIL_RECIPIENT
 	msg = MIMEText(form_body)
 	msg['From'] = mail_from
 	msg['Reply-To'] = mail_from
-	msg['To'] = rcpt_to
+	msg['To'] = gmail_rcpt_to
 	msg['Subject'] = form_subject
 	try:
 		smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
 		smtp_server.starttls()
-		smtp_server.login(GMAIL_LOGIN, GMAIL_PASSWORD)
-		smtp_server.sendmail(mail_from, MAIL_RECIPIENT, msg.as_string())
+		smtp_server.login(gmail_login, gmail_password)
+		smtp_server.sendmail(mail_from, gmail_rcpt_to, msg.as_string())
 		smtp_server.close()
-		redirect(REDIRECT_AFTER_SUCESS)
+		redirect(redirect_after_sucess)
 	except:
 		#raise
-		return "Error connecting to smtp server"
+		abort(500, "Error connecting to smtp server.")
 
 
-run(host=BIND_ADDRESS, port=BIND_PORT)
+run(host=bind_address, port=bind_port)
 
